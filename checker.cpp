bb3c1544eccaf651fa3d38a847e0b8204423d693
HarishMurugancoolz <48434581+HarishMurugancoolz@users.noreply.github.com>
2020-08-29 10:50:59 +0530

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -1,120 +1,83 @@
 #include <iostream>
-
 using namespace std;
 #include <assert.h>
-class Alert
+class AlertAbstract
 {
-    public:
-        virtual void raiseAlert(const char* vitalName,const char* level)=0;
-     
-    
+public:
+	virtual void raiseAlert(const char* vitalName, const char* level) = 0;
+
 };
 
-class AlertWithSMS:public Alert
+class AlertWithSMS :public AlertAbstract
 {
-    public:
-        void raiseAlert(const char* vitalName,const char* level) 
-        {
-            cout<<"SMS:"<<vitalName<<" "<<level<<endl;
-        }
+public:
+	void raiseAlert(const char* vitalName, const char* level)
+	{
+		cout << "SMS:" << vitalName << " " << level << endl;
+	}
 };
-class AlertWithSound:public Alert
+class AlertWithSound :public AlertAbstract
 {
-    public:
-        void raiseAlert(const char* vitalName,const char* level)
-        {
-            cout<<"sound:"<<vitalName<<" "<<level<<endl;
-        }
+public:
+	void raiseAlert(const char* vitalName, const char* level)
+	{
+		cout << "sound:" << vitalName << " " << level << endl;
+	}
 };
+
+class LimitChecker
+{
+	private:
+		float type;
+		float lowerLimt;
+		float upperLimit;
+		AlertAbstract *alert;
+public:
+	bool isLow(float, float);
+	bool isHigh(float, float);
+	bool isInsideLimit(float, float, float);
+
+};
+
+bool LimitChecker::isLow(float value, float lowerLimit)
+{
+	return(value <= lowerLimit);
+}
+bool LimitChecker::isHigh(float value, float upperLimit)
+{
+	return(value >= upperLimit);
+}
+bool LimitChecker::isInsideLimit(float value, float lowerLimit, float upperLimit)
+{
+	return (value >= lowerLimit && value <= upperLimit);
+
+}
 
 class vital
 {
-    Alert *a;
-    
-    public:
-    
-    bool isInsideLimit(float,float ,float );
-    bool isLow(float,float);
-    bool isHigh(float,float);
-    void vitalsAreOk(float, float , float);
-    void bpmCheck(Alert*,float );
-    void spo2Check(Alert*,float );
-    void respRateCheck(Alert*,float );
-        
-    
+	
+	public:
+		void Checker(AlertAbstract*, const char*,float,float,float);
+
+
 };
 
-void vital::bpmCheck(Alert *a,float bpm)
-{
-    if(isInsideLimit(bpm,70,150))
-    {
-        a->raiseAlert("bpm","normal");
-    }
-    else if(isLow(bpm,70))
-    {
-        a->raiseAlert("bpm","LOW");
-    }
-    else if(isHigh(bpm,150))
-    {
-        a->raiseAlert("bpm","High");
-    }
-}
-void vital::spo2Check(Alert *a,float spo2)
-{
-    if(isInsideLimit(spo2,90,100))
-    {
-        a->raiseAlert("spo2","normal");
-    }
-    else if(isLow(spo2,90))
-    {
-        a->raiseAlert("spo2","LOW");
-    }
-    else if(isHigh(spo2,100))
-    {
-        a->raiseAlert("spo2","High");
-    }
-}
-void vital::respRateCheck(Alert *a,float respRate)
-{
-    if(isInsideLimit(respRate,35,90))
-    {
-        a->raiseAlert("respRate","normal");
-    }
-    else if(isLow(respRate,35))
-    {
-        a->raiseAlert("respRate","LOW");
-    }
-    else if(isHigh(respRate,90))
-    {
-        a->raiseAlert("respRete","High");
-    }
-}
 
-bool vital::isLow(float value,float lowerLimit)
+void vital::Checker(AlertAbstract *a, const char* type ,float value,float lowerLimit, float  upperLimit)
 {
-    return(value<=lowerLimit);
-}
-bool vital::isHigh(float value,float upperLimit)
-{
-    return(value>=upperLimit);
-}
-bool vital::isInsideLimit(float value,float lowerLimit,float upperLimit)
-{
-  return (value>= lowerLimit && value <=upperLimit);
-  
-}
+	LimitChecker lm;
+	if (lm.isHigh(value, upperLimit))
+		a->raiseAlert(type, "HIGH");
+	else if(lm.isLow(value, lowerLimit))
+		a->raiseAlert(type, "LOW");
 
-void vital::vitalsAreOk(float bpm, float spo2, float respRate) {
-  
-  Alert *a=new AlertWithSMS;
-  bpmCheck(a,bpm);
-  spo2Check(a,spo2);
-  respRateCheck(a,respRate);
+
 }
 
 int main() {
-  
-  vital v;
-  v.vitalsAreOk(80,90,180);
-  
+
+	vital v;
+	AlertAbstract *a = new AlertWithSMS;
+	v.Checker(a, "bpm", 30, 65, 150);
+
 }
