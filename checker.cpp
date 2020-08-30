19e2d417ad54bd238c53a791e9943462ac159be1
HarishMurugancoolz <48434581+HarishMurugancoolz@users.noreply.github.com>
2020-08-30 17:05:38 +0530

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -1,27 +1,28 @@
 #include <iostream>
+#include <string>
 using namespace std;
 #include <assert.h>
 class AlertAbstract
 {
 public:
-	virtual void raiseAlert(const char* vitalName, const char* level) = 0;
+	virtual void raiseAlert(string id,const char* vitalName, const char* level) = 0;
 
 };
 
 class AlertWithSMS :public AlertAbstract
 {
 public:
-	void raiseAlert(const char* vitalName, const char* level)
+	void raiseAlert(string id,const char* vitalName, const char* level) override
 	{
-		cout << "SMS:" << vitalName << " " << level << endl;
+		cout << "SMS:" << "person"<<" "<<id<<vitalName << " " << level << endl;
 	}
 };
 class AlertWithSound :public AlertAbstract
 {
 public:
-	void raiseAlert(const char* vitalName, const char* level)
+	void raiseAlert(string id,const char* vitalName, const char* level) override
 	{
-		cout << "sound:" << vitalName << " " << level << endl;
+		cout << "sound:" <<"person" <<" "<<id<<vitalName << " " << level << endl;
 	}
 };
 
@@ -32,17 +33,16 @@ private:
 	float lowerLimit;
 	float upperLimit;
 public:
-	LimitChecker(AlertAbstract *alert,const char* type, float value,float lowerLimit, float upperLimit) :
+	LimitChecker(const char* type,float lowerLimit, float upperLimit) :
 		type(type),
 		lowerLimit(lowerLimit),
 		upperLimit(upperLimit)
 	{
-		this->Checker(alert, value);
 	}
 	bool isLow(float, float);
 	bool isHigh(float, float);
 	bool isInsideLimit(float, float, float);
-	void Checker(AlertAbstract*, float);
+	void Checker(string id,AlertAbstract*, float);
 
 };
 
@@ -59,26 +59,79 @@ bool LimitChecker::isInsideLimit(float value, float lowerLimit, float upperLimit
 	return (value >= lowerLimit && value <= upperLimit);
 
 }
-void LimitChecker::Checker(AlertAbstract *a,float value)
+void LimitChecker::Checker(string id,AlertAbstract *a,float value)
 {
 	if (this->isHigh(value, this->upperLimit))
-		a->raiseAlert(this->type, "HIGH");
+		a->raiseAlert(id,this->type, "HIGH");
 	else if (this->isLow(value, this->lowerLimit))
-		a->raiseAlert(this->type, "LOW");
-}
+		a->raiseAlert(id,this->type, "LOW");
 
+
+}
+struct PersonsVital
+{
+
+	string id;//id or name
+	AlertAbstract *alert;
+	float bpmValue;
+	float spo2Value;
+	float respRateValue;
+
+};
 
 class vitalIntegrator
 {
 	LimitChecker bpmCheck, spo2Check, respRateCheck;
 public:
-	vitalIntegrator(AlertAbstract* alert, float bpm, float spo2, float respRate) :bpmCheck(alert,"bpm", bpm,70, 150),
-		spo2Check(alert,"spo2",spo2 ,90, 100),
-		respRateCheck(alert,"respRate", respRate,35, 90){}
+	vitalIntegrator():bpmCheck("bpm",70, 150),
+		spo2Check("spo2",90, 100),
+		respRateCheck("respRate",35,90)
+		
+	{
+		
+				
+
+	}
+	void checkAllVitals(PersonsVital p[], int n_person)
+	{
+		for (int i_person = 0; i_person < n_person; i_person++)
+		{
+			bpmCheck.Checker(p[i_person].id, p[i_person].alert, p[i_person].bpmValue);
+			spo2Check.Checker(p[i_person].id, p[i_person].alert, p[i_person].spo2Value);
+			respRateCheck.Checker(p[i_person].id, p[i_person].alert, p[i_person].bpmValue);
+		}
+	}
+
+
 };
 int main() {
+	PersonsVital p[2];
+	for (int i = 0; i < 2; i++)
+	{
+		p[i].id = to_string(i+1);
+		cout << "Enter the Alert type integer 0:AlertWithSms 1:AlertWithSound" << endl;
+		int alert;
+		cin >> alert;
+		if (alert == 0)
+		{
+			p[i].alert = new AlertWithSMS;
+		}
+		else
+		{
+			p[i].alert = new AlertWithSound;
+		}
+
+		cout << "Enter the Bpm value" << endl;
+		cin >> p[i].bpmValue;
+		cout << "Enter the spo2 value" << endl;
+		cin >> p[i].spo2Value;
+		cout << "Enter the respRate value" << endl;
+		cin >> p[i].respRateValue;
+		
+	}
+	vitalIntegrator v1;
+	v1.checkAllVitals(p, 2);
+	
 
 	
-	AlertAbstract *a = new AlertWithSMS;
-	vitalIntegrator v(a,80,90,170);
 }
