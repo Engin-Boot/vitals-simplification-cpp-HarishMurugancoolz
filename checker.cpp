0ea1dd4c32a14d3e8eed92d1a47ea3a58804f0fa
HarishMurugancoolz <48434581+HarishMurugancoolz@users.noreply.github.com>
2020-08-31 18:40:23 +0530

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -1,30 +1,29 @@
 #include <iostream>
 #include <string>
-#include <iostream>
-#include <string>
+#include<vector>
 using namespace std;
 #include <assert.h>
 class AlertAbstract
 {
 public:
-	virtual void raiseAlert(string id,const char* vitalName, const char* level) = 0;
+	virtual void raiseAlert(string id, const char* vitalName, const char* level) = 0;
 
 };
 
 class AlertWithSMS :public AlertAbstract
 {
 public:
-	void raiseAlert(string id,const char* vitalName, const char* level) override
+	void raiseAlert(string id, const char* vitalName, const char* level) override
 	{
-		cout << "SMS:" << "person"<<" "<<id<<vitalName << " " << level << endl;
+		cout << "SMS:" << "person" << " " << id << vitalName << " " << level << endl;
 	}
 };
 class AlertWithSound :public AlertAbstract
 {
 public:
-	void raiseAlert(string id,const char* vitalName, const char* level) override
+	void raiseAlert(string id, const char* vitalName, const char* level) override
 	{
-		cout << "sound:" <<"person" <<" "<<id<<vitalName << " " << level << endl;
+		cout << "sound:" << "person" << " " << id << vitalName << " " << level << endl;
 	}
 };
 class LimitChecker
@@ -34,7 +33,7 @@ private:
 	float lowerLimit;
 	float upperLimit;
 public:
-	LimitChecker(const char* type,float lowerLimit, float upperLimit) :
+	LimitChecker(const char* type, float lowerLimit, float upperLimit) :
 		type(type),
 		lowerLimit(lowerLimit),
 		upperLimit(upperLimit)
@@ -43,7 +42,7 @@ public:
 	bool isLow(float, float);
 	bool isHigh(float, float);
 	bool isInsideLimit(float, float, float);
-	void Checker(string id,AlertAbstract*, float);
+	void Checker(string id, AlertAbstract*, float);
 
 };
 
@@ -60,21 +59,19 @@ bool LimitChecker::isInsideLimit(float value, float lowerLimit, float upperLimit
 	return (value >= lowerLimit && value <= upperLimit);
 
 }
-void LimitChecker::Checker(string id,AlertAbstract *a,float value)
+void LimitChecker::Checker(string id, AlertAbstract *a, float value)
 {
 	if (this->isHigh(value, this->upperLimit))
-		a->raiseAlert(id,this->type, "HIGH");
+		a->raiseAlert(id, this->type, "HIGH");
 	else if (this->isLow(value, this->lowerLimit))
-		a->raiseAlert(id,this->type, "LOW");
-
-
+		a->raiseAlert(id, this->type, "LOW");
 }
 struct PersonsVital
 {
 
 	string id;//id or name
 	AlertAbstract *alert;
-	float vitalValue[3];
+	vector<float> vitalValue;
 
 };
 
@@ -82,17 +79,17 @@ class vitalIntegrator
 {
 	LimitChecker bpmCheck, spo2Check, respRateCheck;
 public:
-	vitalIntegrator():bpmCheck("bpm",70,150),
-		spo2Check("spo2",90,100),
-		respRateCheck("respRate",35,90)
-		
+	vitalIntegrator() :bpmCheck("bpm", 70, 150),
+		spo2Check("spo2", 90, 100),
+		respRateCheck("respRate", 35, 90)
+
 	{
-				
+
 
 	}
-	void checkAllVitals(PersonsVital p[],int n_persons)
+	void checkAllVitals(vector<PersonsVital> p)
 	{
-		for (int i_person = 0; i_person <n_persons; i_person++)
+		for (unsigned int i_person = 0; i_person <p.size(); i_person++)
 		{
 			bpmCheck.Checker(p[i_person].id, p[i_person].alert, p[i_person].vitalValue[0]);
 			spo2Check.Checker(p[i_person].id, p[i_person].alert, p[i_person].vitalValue[1]);
@@ -104,7 +101,7 @@ public:
 };
 
 int main() {
-	PersonsVital p[2];
+	vector<PersonsVital> p(2);
 	for (int i = 0; i < 2; i++)
 	{
 		p[i].id = to_string(i + 1);
@@ -120,18 +117,22 @@ int main() {
 			p[i].alert = new AlertWithSound;
 		}
 		cout << "Enter the Bpm value" << endl;
-		cin >> p[i].vitalValue[0];
+		float bpmValue, spo2Value, respRateValue;
+		cin >> bpmValue;
+		p[i].vitalValue.push_back(bpmValue);
 		cout << "Enter the spo2 value" << endl;
-		cin >> p[i].vitalValue[1];
+		cin >> spo2Value;
+		p[i].vitalValue.push_back(spo2Value);
 		cout << "Enter the respRate value" << endl;
-		cin >> p[i].vitalValue[2];
+		cin >> respRateValue;
+		p[i].vitalValue.push_back(respRateValue);
 
 
 	}
 	vitalIntegrator v1;
-	v1.checkAllVitals(p,2);
+	v1.checkAllVitals(p);
 	return 0;
-	
 
-	
+
+
 }
